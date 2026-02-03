b3511daee9bbc4c72159e526be6705074d66b767
Jibril Kalifa <120446317+Jibrilkelifa@users.noreply.github.com>
2026-02-03 10:44:12 +0300

    feat: cert-card component added

--- src/app/shared/components/cert-card/cert-card.ts
+++ src/app/shared/components/cert-card/cert-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Certification } from '../../../data/certifications.data';
 
 @Component({
   selector: 'app-cert-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './cert-card.html',
-  styleUrl: './cert-card.css',
+  styleUrls: ['./cert-card.scss']
 })
-export class CertCard {
-
+export class CertCardComponent {
+  @Input({ required: true }) cert!: Certification;
 }
